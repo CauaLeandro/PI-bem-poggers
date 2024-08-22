8041680c4926be9606632901c4523ebb702d9532
Bernardorocha0710 <bernardo.senac.0710@gmail.com>
2024-08-22 15:23:33 -0300

    goofy ahh walk

--- Eu adoro roblox2/Assets/script/MazeShotter.cs
+++ Eu adoro roblox2/Assets/script/MazeShotter.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MazeShotter : MonoBehaviour
 {
+
+
     public Camera sceneCamera;
 
     public float moveSpeed;
@@ -11,7 +13,9 @@ public class MazeShotter : MonoBehaviour
     private Vector2 MoveDirection;
     public Rigidbody2D rb;
 
-    private Vector2 mouseDirection;
+
+
+    private Vector2 moveDirection;
 
     private Vector2 mousePosition;
 
@@ -30,10 +34,17 @@ public class MazeShotter : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         MoveDirection = new Vector2(moveX, moveY);
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void Move()
     {
         rb.velocity = new Vector2(MoveDirection.x * moveSpeed, MoveDirection.y * moveSpeed);
+
+        // gira o player
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
