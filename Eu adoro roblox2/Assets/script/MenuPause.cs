3ff388a88b49e2b450fa5732a9fab791fda77a0c
Gyakko07 <caualeandrocoelho0@gmail.com>
2024-11-13 13:11:39 -0300

    Não sei

--- Eu adoro roblox2/Assets/script/MenuPause.cs
+++ Eu adoro roblox2/Assets/script/MenuPause.cs
@@ -11,17 +11,23 @@ public class MenuPause : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             menu.SetActive(true);
-            Time.timeScale = 0.0f;
+            Time.timeScale = 0;
         }
     }
     public void SairPause()
     {
         menu.SetActive(false);
-        Time.timeScale = 1.0f;
+        Time.timeScale = 1;
+        Debug.Log("cu");
     }
     public void SairProMenu()
     {
         SceneManager.LoadScene("Menu");
-        Time.timeScale = 1.0f;
+        Time.timeScale = 1;
+        Debug.Log("cuzao");
+    }
+    public void RestartGay()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
